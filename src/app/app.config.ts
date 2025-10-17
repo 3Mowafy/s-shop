4a490e1e9d0b0b7af6c86c5ad17e8d763314991e
3Mowafy <3mowafy@gmail.com>
2025-10-18 00:55:42 +0300

    .

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  provideRouter,
+  withHashLocation,
+  withRouterConfig,
+} from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
@@ -17,7 +21,7 @@ import {
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(
       withInterceptors([
         credentialsInterceptor,
